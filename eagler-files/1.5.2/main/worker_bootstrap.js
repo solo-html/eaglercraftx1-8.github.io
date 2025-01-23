801bd97d94355051b019b64c3fbae0056660ef8f
PrestonT500 <preston.truong1@outlook.com>
2025-01-22 18:36:20 -0800

    Added EaglerForge EFI (v2.6 w/ u43) While keeping the other eaglerforge as it is non EFI
    Signed-off-by: PrestonT500 <preston.truong1@outlook.com>

--- eagler-files/1.5.2/main/worker_bootstrap.js
+++ eagler-files/1.5.2/main/worker_bootstrap.js
@@ -1,5 +1,3 @@
-onmessage = function(o) {
-	importScripts("classes_server.js");
-	eaglercraftServerOpts = o.data;
-	main();
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:16a1195ced8dbdade8855a07cfbc4cc4809a6532e10eff1dfb47f37a584c5bdd
+size 113
--- eagler-files/1.8/resent4.0/README.md
+++ eagler-files/1.8/resent4.0/README.md
@@ -1,2 +1,3 @@
-# Resent4-Patch4
-Resent Client 4.0 Patch 4
+version https://git-lfs.github.com/spec/v1
+oid sha256:d3aebe4a02f93d9fed62d478f64a39bae6454e58a7e58c94784d3448c5a5fd1d
+size 45
